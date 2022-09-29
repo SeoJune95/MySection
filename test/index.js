9dd07d8340a3bff829374bcf965290708bd9674c
SeoJune95 <112795728+SeoJune95@users.noreply.github.com>
2022-09-29 21:50:25 +0900

    Create index.js

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var valueById = $('.text-field').val();
+function noText(){
+
+    var valueById = $('.text-field').val();
+    if(valueById == 'undefined'){
+        $('.text-field').css('fontColor','9b9b9b');
+    }
+
+}
