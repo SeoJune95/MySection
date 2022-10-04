2305557ce614d511e01f0da2ce90f1f3b17d802c
SeoJune95 <cuskastie@gmail.com>
2022-10-04 15:09:19 +0900

    update

---
+++ JAVASCRIPT4/index.js
@@ -0,0 +1,15 @@
+$('#idSection').on('input',checkVal);
+$('#pwSection').on('input',checkVal);
+
+function checkVal() {
+    var textId = $('#idSection').val();
+    var textPw = $('#pwSection').val();
+
+    if(textId != "" && textPw != "") {
+        $('#button').css('backgroundColor','#1bbc9b');
+<!--        $('#button').removeAttr('disabled');-->
+    }else{
+        $('#button').css('backgroundColor','#9b9b9b');
+<!--        $('#button').attr('disabled','disabled');-->
+    }
+}
